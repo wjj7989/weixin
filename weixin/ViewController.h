1998f71669fe258f6726356d5f181bbac60c9c10
Halt <454752040@qq.com>
2015-08-07 16:18:18 +0800

    Initial Commit

---
+++ weixin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weixin
+//
+//  Created by gs_mac_wjj on 15/8/7.
+//  Copyright (c) 2015年 Nil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weixin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weixin
+//
+//  Created by gs_mac_wjj on 15/8/7.
+//  Copyright (c) 2015年 Nil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
